83530b35a7904464df57519abd8c33e43291cb98
user <vovafigvam@gmail.com>
2026-04-04 14:38:55 +0300

    Fix task 1

--- 3822B1FI2/1_permutations_cxx/sdobnov_vladimir/permutations_cxx.cpp
+++ 3822B1FI2/1_permutations_cxx/sdobnov_vladimir/permutations_cxx.cpp
@@ -3,29 +3,50 @@
 #include <unordered_map>
 #include <array>
 #include <algorithm>
+#include <string>
+
+static std::string make_key(const std::string& word) {
+    std::array<int, 26> freq = { 0 };
+
+    for (char c : word) {
+        freq[c - 'a']++;
+    }
+
+    std::string key;
+    key.reserve(26 * 2);
+
+    for (int f : freq) {
+        key += std::to_string(f);
+        key += '#';
+    }
+
+    return key;
+}
 
 void Permutations(dictionary_t& dictionary) {
-	std::unordered_map<std::string, std::vector<std::string>> groups;
-	std::unordered_map<std::string, std::string> cache;
+    std::unordered_map<std::string, std::vector<std::string>> groups;
+    std::unordered_map<std::string, std::string> cache;
 
-	for (const auto& [word, _] : dictionary) {
-		auto key = make_key(word);
-		cache[word] = key;
-		groups[key].push_back(word);
-	}
+    for (const auto& [word, _] : dictionary) {
+        auto key = make_key(word);
+        cache[word] = key;
+        groups[key].push_back(word);
+    }
 
-	for (auto& [word, vec] : dictionary) {
-		const auto& key = cache[word];
-		const auto& group = groups[key];
+    for (auto& [word, vec] : dictionary) {
+        const auto& key = cache[word];
+        const auto& group = groups[key];
 
-		vec.reserve(group.size() > 0 ? group.size() - 1 : 0);
+        vec.clear();
 
-		for (const auto& other : group) {
-			if (other != word) {
-				vec.push_back(other);
-			}
-		}
+        vec.reserve(group.size() > 0 ? group.size() - 1 : 0);
 
-		std::sort(vec.rbegin(), vec.rend());
-	}
+        for (const auto& other : group) {
+            if (other != word) {
+                vec.push_back(other);
+            }
+        }
+
+        std::sort(vec.rbegin(), vec.rend());
+    }
 }
